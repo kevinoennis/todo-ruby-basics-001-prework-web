ccd9e3af8ffd2ca2f81269fd1854015a5619d73e
kevinoennis <kevinoennis@gmail.com>
2016-03-24 12:25:00 +0000

    nitrous-backup at Thu Mar 24 12:25:00 UTC 2016

--- lib/ruby_basic.rb
+++ lib/ruby_basic.rb
@@ -1,27 +1,16 @@
+##editme
+
 def division(num1, num2)
- 
 end
-
 def assign_variable(value)
-  
 end
-
 def argue
-  
 end
-
 def greeting
-
 end
-
 def return_a_value
-
 end
-
 def last_evaluated_value
-
 end
-
 def pizza_party
-
 end
